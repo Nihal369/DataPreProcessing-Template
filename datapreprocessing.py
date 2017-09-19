b2a2b32470316c51552c7246544c2186d7b9170a
nihal369 <nihalismailk@gmail.com>
2017-09-19 21:42:25 +0530

    Classified data

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -4,18 +4,30 @@ Created on Tue Sep 19 18:51:37 2017
 
 @author: nihal369
 """
-
+#Importing the librarires
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
+#Importing the dataset
 dataset=pd.read_csv('Data.csv')
 x = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,-1].values
 
+#Adding missing values
 from sklearn.preprocessing import Imputer
 imputer= Imputer(missing_values="NaN",strategy="mean",axis=0)
 imputer=imputer.fit(x[:, 1:3])
 x[:,1:3]=imputer.transform(x[:, 1:3])
+
+#Catogerising data(Convert all text to int)
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+labelEncoder_x = LabelEncoder()
+x[:,0]=labelEncoder_x.fit_transform(x[:,0]);
+oneHotEncoder=OneHotEncoder(categorical_features=[0])
+x=oneHotEncoder.fit_transform(x).toarray()
+labelEncoder_y = LabelEncoder()
+y=labelEncoder_y.fit_transform(y)
+
+
 
