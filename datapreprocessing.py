7554ec067d5b89b6ac6ee51d06ae8bdf76bb0668
nihal369 <nihalismailk@gmail.com>
2017-09-19 22:19:24 +0530

    Final template :dancer:

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -15,19 +15,19 @@ x = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,-1].values
 
 #Adding missing values
-from sklearn.preprocessing import Imputer
+'''from sklearn.preprocessing import Imputer
 imputer= Imputer(missing_values="NaN",strategy="mean",axis=0)
 imputer=imputer.fit(x[:, 1:3])
-x[:,1:3]=imputer.transform(x[:, 1:3])
+x[:,1:3]=imputer.transform(x[:, 1:3])'''
 
 #Catogerising data(Convert all text to int)
-from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+'''from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 labelEncoder_x = LabelEncoder()
 x[:,0]=labelEncoder_x.fit_transform(x[:,0]);
 oneHotEncoder=OneHotEncoder(categorical_features=[0])
 x=oneHotEncoder.fit_transform(x).toarray()
 labelEncoder_y = LabelEncoder()
-y=labelEncoder_y.fit_transform(y)
+y=labelEncoder_y.fit_transform(y)'''
 
 #Making of Training and Test sets
 from sklearn.cross_validation import train_test_split
